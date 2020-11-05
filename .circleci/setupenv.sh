040299c7414e4e6c560040658a929e4f646d11bb
Aycan Irican <aycan.irican@coinberry.com>
2020-11-05 00:01:52 -0800

    use company image

--- .circleci/setupenv.sh
+++ .circleci/setupenv.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-apt -y update
-apt -y install hlint apt-transport-https ca-certificates wget gnupg
+set -ex
 
-# Setup mock rippled server
 wget -q -O - "https://repos.ripple.com/repos/api/gpg/key/public" | apt-key add -
-echo "deb https://repos.ripple.com/repos/rippled-deb bionic stable" | sudo tee -a /etc/apt/sources.list.d/ripple.list
-apt -y update
-apt -y install rippled
+# check ripple repo fingerprint
+apt-key finger | grep "C001 0EC2 05B3 5A33 10DC  90DE 395F 97FF CCAF D9A"
+
+echo "deb https://repos.ripple.com/repos/rippled-deb xenial stable" | tee -a /etc/apt/sources.list.d/ripple.list
+
+apt-get -y update
+apt-get -y install rippled
 
 # Setup mock parity server
 curl -O https://releases.parity.io/ethereum/v2.5.9/x86_64-unknown-linux-gnu/parity
